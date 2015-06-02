2ff572c61d1c7209e1069efde1f69bb1526031f5
calebgregory <calebgregory@gmail.com>
2015-06-02 15:49:40 -0500

    Adding form and table to index.jade and angular logic to main.js

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+angular
+  .module('addressBook', [])
+
+  .controller('Main', function() {
+    var main = this;
+
+    main.people = [
+      {
+        name    : "Alex",
+        twitter : "@al",
+        phone   : 1234567890
+      },
+      {
+        name    : "Bessa",
+        twitter : "@olebess",
+        phone   : 3216540987
+      },
+      {
+        name    : "Creotine",
+        twitter : "@crawdaddy",
+        phone   : 3241560798
+      },
+      {
+        name    : "Dugger",
+        twitter : "@duggy",
+        phone   : 3124657089
+      }
+    ];
+
+    main.newEntry = {};
+
+    main.addEntry = function() {
+      main.people.push(main.newEntry);
+      main.newEntry = {};
+    };
+
+    main.removeEntry = function(person) {
+      main.people.splice(main.people.indexOf(person), 1)
+    };
+  })
